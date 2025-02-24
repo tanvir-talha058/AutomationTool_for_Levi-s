5b3def320f73c85a248170036aa5e56b4316d338
Tanvir Ahmed <110832885+tanvir-talha058@users.noreply.github.com>
2025-02-24 21:59:11 +0600

    Update automation.py

--- automation.py
+++ automation.py
@@ -17,7 +17,6 @@ class WebAutomationTool:
         self.input_box = None
         self.submit_button = None
 
-        
         # Header 
         self.header = tk.Label(root, text="Web Automation", bg="#3B7097", fg="white", font=("Arial", 14, "bold"), padx=10, pady=5)
         self.header.pack(fill=tk.X)
